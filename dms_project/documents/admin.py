520b36d090c204d12b3934ca03b224f7f14d13b3
krishraj <kdgohil1819@gmail.com>
2026-02-14 17:03:37 +0530

    demo server run

--- dms_project/documents/admin.py
+++ dms_project/documents/admin.py
@@ -1,3 +1,9 @@
+# documents/admin.py
 from django.contrib import admin
+from .models import Document
 
-# Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'uploader', 'status', 'uploaded_at']
+    list_filter = ['status', 'category']
+    search_fields = ['title', 'author', 'description']
--- dms_project/review/admin.py
+++ dms_project/review/admin.py
@@ -1,3 +1,9 @@
+# review/admin.py
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['document', 'reviewer', 'status', 'reviewed_at']
+    list_filter = ['status']
+    search_fields = ['document__title', 'comments']
